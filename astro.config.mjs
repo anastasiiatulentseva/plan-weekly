a566d110702962767a48421b23d7ea5551dd4c75
Anastasia Tulentseva <anastasiia.tulentseva@gmail.com>
2026-09-02 18:47:37 +0100

    Configure GitHub Pages demo build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://anastasiiatulentseva.github.io",
+  ...(process.env.GITHUB_PAGES === "true" ? { base: "/plan-weekly" } : {}),
   integrations: [react()],
   output: "static"
 });
